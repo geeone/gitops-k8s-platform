dbbaf4c30d9fe3675afe23767ac4e4b66ec965e7
Sergei Denisenko <8565196+geeone@users.noreply.github.com>
2025-12-01 10:59:10 +0100

    feat(flux): enable image automation controllers in Terraform bootstrap

--- terraform/flux-bootstrap.tf
+++ terraform/flux-bootstrap.tf
@@ -1,6 +1,7 @@
 // Bootstrap Flux and sync from clusters/staging
 resource "flux_bootstrap_git" "this" {
   embedded_manifests = true
+  components_extra   = ["image-reflector-controller", "image-automation-controller"]
   path               = "clusters/staging"
 
   depends_on = [module.eks]
