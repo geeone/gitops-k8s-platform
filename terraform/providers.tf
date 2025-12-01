6ce83b3772c768e0ac751b415b025c282c75b4e3
Sergei Denisenko <8565196+geeone@users.noreply.github.com>
2025-12-01 16:12:12 +0100

    fix(terraform): update Helm provider definition

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -17,7 +17,7 @@ provider "kubernetes" {
 
 // Helm provider (for Helm charts installs)
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.cluster.token
